6145cb44a75c9c195a7a812944e4bc3d73c17e60
alexey <ale441v@gmail.com>
2018-07-20 12:50:48 +0300

    Walking through all the repositories belonging to a given organization

--- entrypoint.php
+++ entrypoint.php
@@ -4,11 +4,12 @@ require 'vendor/autoload.php';
 
 $client = new \Github\Client();
 
-$username = 'organizationname';
-$path = 'README.md';
+$username = 'username';
+$organization = 'organization';
+$path = 'composer.json';
 $repo = 'testrepo';
 $token = '****';
-$newBranchName = 'new-branch1';
+$newBranchName = 'new-branch100501';
 $pullRequestBody = 'Pull request body';
 $pullRequestTitle = 'Pull request title';
 $possibleBaseBranches = array('develop', 'dev', 'master');
@@ -16,26 +17,8 @@ $dependency = 'knplabs/github-api';
 $onlyIfOldVersionEqualsTo = '^2.8';
 $newVersion = '^100500.8';
 
-$client->authenticate($token, null, \Github\Client::AUTH_HTTP_TOKEN);
-
-/** @var \Github\Api\Repo $repoApi */
-$repoApi = $client->api('repo');
-
-$oldFileContentBase64 = $repoApi->contents()->show($username, $repo, $path);
-$oldFileContent = base64_decode($oldFileContentBase64['content']);
-
-$oldFileContent = json_decode($oldFileContent, true);
-// @todo handle NULL
-
-$composerJsonReplacer = new \VersionPullRequester\ComposerJsonReplacer();
-$newFileContent = $composerJsonReplacer->replaceVersionOfDependency($oldFileContent, $dependency, $newVersion, $onlyIfOldVersionEqualsTo);
-
-/** @var \Github\Api\GitData $gitDataApi */
-$gitDataApi = $client->api('gitData');
-
-$branches = $gitDataApi->references()->branches($username, $repo);
-
-function chooseBaseBranch(array $branches, $possibleBaseBranches, &$chosenBranch) {
+function chooseBaseBranch(array $branches, array $possibleBaseBranches, &$chosenBranch)
+{
     $branchPrefixInRef = 'refs/heads/';
 
     foreach ($possibleBaseBranches as $possibleBaseBranch) {
@@ -50,27 +33,50 @@ function chooseBaseBranch(array $branches, $possibleBaseBranches, &$chosenBranch
     return null;
 }
 
-$branchShaToForkFrom = chooseBaseBranch($branches, $possibleBaseBranches,$chosenBranch);
+$client->authenticate($token, null, \Github\Client::AUTH_HTTP_TOKEN);
 
-$referenceData = ['ref' => 'refs/heads/' . $newBranchName, 'sha' => $branchShaToForkFrom];
-$reference = $gitDataApi->references()->create($username, $repo, $referenceData);
+/** @var \Github\Api\User $userApi */
+$userApi = $client->api('user');
+$repos = $userApi->repositories($organization);
 
-$result = $repoApi->contents()->update(
-    $username,
-    $repo,
-    $path,
-    $newFileContent,
-    'commit',
-    $oldFileContentBase64['sha'],
-    $newBranchName
-);
+foreach ($repos as $repoParams) {
+    $repo = $repoParams['name'];
 
-/** @var \Github\Api\PullRequest $pullRequestApi */
-$pullRequestApi = $client->api('pull_request');
+    /** @var \Github\Api\Repo $repoApi */
+    $repoApi = $client->api('repo');
 
-$pullRequest = $pullRequestApi->create($username, $repo, array(
-    'base'  => $chosenBranch,
-    'head'  => $newBranchName,
-    'title' => $pullRequestTitle,
-    'body'  => $pullRequestBody,
-));
+    $oldFileContentBase64 = $repoApi->contents()->show($organization, $repo, $path);
+    $oldFileContent = base64_decode($oldFileContentBase64['content']);
+
+    $oldFileContent = json_decode($oldFileContent, true);
+    // @todo handle NULL
+
+    $composerJsonReplacer = new \VersionPullRequester\ComposerJsonReplacer();
+    $newFileContent = $composerJsonReplacer->replaceVersionOfDependency($oldFileContent, $dependency, $newVersion, $onlyIfOldVersionEqualsTo);
+    $newFileContent = json_encode($newFileContent);
+
+    /** @var \Github\Api\GitData $gitDataApi */
+    $gitDataApi = $client->api('gitData');
+    $branches = $gitDataApi->references()->branches($organization, $repo);
+    $branchShaToForkFrom = chooseBaseBranch($branches, $possibleBaseBranches, $chosenBranch);
+    $referenceData = ['ref' => 'refs/heads/' . $newBranchName, 'sha' => $branchShaToForkFrom];
+    $reference = $gitDataApi->references()->create($organization, $repo, $referenceData);
+    $result = $repoApi->contents()->update(
+        $organization,
+        $repo,
+        $path,
+        $newFileContent,
+        'commit',
+        $oldFileContentBase64['sha'],
+        $newBranchName
+    );
+
+    /** @var \Github\Api\PullRequest $pullRequestApi */
+    $pullRequestApi = $client->api('pull_request');
+    $pullRequest = $pullRequestApi->create($organization, $repo, array(
+        'base' => $chosenBranch,
+        'head' => $newBranchName,
+        'title' => $pullRequestTitle,
+        'body' => $pullRequestBody,
+    ));
+}
